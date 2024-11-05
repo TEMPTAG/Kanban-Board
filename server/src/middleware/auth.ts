78b9a82e9380d5b3b222f024a5bd7a7c0f42eeef
Ian Ferguson <iansterlingferguson@gmail.com>
2024-11-04 17:15:23 -0700

    auth.ts updates.

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,10 +1,36 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
 interface JwtPayload {
   username: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   // TODO: verify the token exists and add the user data to the request object
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
+    if (err) {
+      return res.status(403).send("Forbidden");
+    }
+
+    if (!user) {
+      return res.status(401).send("Unauthorized");
+    }
+
+    req.user = user as JwtPayload;
+    next();
+    return;
+  });
+
+  return;
 };
